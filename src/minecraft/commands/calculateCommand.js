15faf3fc08ddce25dd722f2f2a371eb30575507e
DS Software <artemsoftware@mail.ru>
2024-03-08 19:30:22 +0400

    ugly formatting + debug

--- src/minecraft/commands/calculateCommand.js
+++ src/minecraft/commands/calculateCommand.js
@@ -3,32 +3,33 @@ const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const mathjs = require('mathjs');
 
 class CalculateCommand extends minecraftCommand {
-  constructor(minecraft) {
-    super(minecraft);
+    constructor(minecraft) {
+        super(minecraft);
 
-    this.name = "calculate";
-    this.aliases = ["calc", "math"];
-    this.description = "Evaluate math expression.";
-    this.options = [
-      {
-        name: "expression",
-        description: "Any kind of math equation",
-        required: true,
-      },
-    ];
-  }
-
-  onCommand(username, message) {
-    try {
-      const calculation = message;
-      const answer = mathjs.format(mathjs.evaluate(calculation), {precision: 3}).toString();
-      
-
-      this.send(`/gc ${username}, the answer is '${answer}'`);
-    } catch (error) {
-      this.send(`/gc [ERROR] The expression cannot be evaluated.`);
+        this.name = "calculate";
+        this.aliases = ["calc", "math"];
+        this.description = "Evaluate math expression.";
+        this.options = [
+            {
+                name: "expression",
+                description: "Any kind of math equation",
+                required: true,
+            },
+        ];
     }
-  }
+
+    onCommand(username, message) {
+        try {
+            const calculation = message;
+            const answer = mathjs.format(mathjs.evaluate(calculation), { precision: 3 }).toString();
+
+
+            this.send(`/gc ${username}, the answer is '${answer}'`);
+        } catch (error) {
+            console.log(error);
+            this.send(`/gc [ERROR] The expression cannot be evaluated.`);
+        }
+    }
 }
 
 module.exports = CalculateCommand;
