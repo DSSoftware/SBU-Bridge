893891d195f2d0da6a4e9e6704e253709e06acda
DS Software <artemsoftware@mail.ru>
2024-03-15 06:44:20 +0400

    added fail catch for blacklist

--- API/utils/blacklist.js
+++ API/utils/blacklist.js
@@ -3,32 +3,37 @@ const axios = require("axios");
 
 async function checkBlacklist(uuid) {
   return new Promise(async (resolve, reject) => {
-    if (config.minecraft.API.banlist.enabled === false) {
-      resolve(false);
-      return;
-    }
-    const API_URL = config.minecraft.API.banlist.URL;
-
-    await axios.get(API_URL, {
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
-
-    let blacklisted = false;
-
-    axios
-      .get(API_URL + `?uuid=${uuid}`)
-      .then(function (response) {
-        if (response.data.banned) {
-          blacklisted = true;
-        }
-        resolve(blacklisted);
-      })
-      .catch(function (error) {
-        blacklisted = true;
-        resolve(blacklisted);
+    try{
+      if (config.minecraft.API.banlist.enabled === false) {
+        resolve(false);
+        return;
+      }
+      const API_URL = config.minecraft.API.banlist.URL;
+  
+      await axios.get(API_URL, {
+        headers: {
+          "Content-type": "application/json",
+        },
       });
+  
+      let blacklisted = false;
+  
+      axios
+        .get(API_URL + `?uuid=${uuid}`)
+        .then(function (response) {
+          if (response.data.banned) {
+            blacklisted = true;
+          }
+          resolve(blacklisted);
+        })
+        .catch(function (error) {
+          blacklisted = true;
+          resolve(blacklisted);
+        });
+    }
+    catch(e){
+      resolve(false);
+    }
   });
 }
 
