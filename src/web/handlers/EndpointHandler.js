677b6f31b45924fe0f8a56bfe59c65736619c0ad
DS Software <artemsoftware@mail.ru>
2024-03-16 16:49:17 +0400

    new endpoint stuff!

--- src/web/handlers/EndpointHandler.js
+++ src/web/handlers/EndpointHandler.js
@@ -2,6 +2,7 @@ const config = require("../../../config.js");
 const { getUUID } = require("../../contracts/API/PlayerDBAPI");
 const Skykings = require("../../../API/utils/skykings");
 const Blacklist = require("../../../API/utils/blacklist");
+const SCFBlacklist = require("../../../API/utils/scfBlacklist");
 
 class EndpointHandler {
   constructor(server) {
@@ -10,15 +11,19 @@ class EndpointHandler {
 
   registerEvents() {
     const { web } = this.server;
-    const guild = config.minecraft.guild.guildName;
+    const guild = config.minecraft.bot.unique_id;
     web.post("/" + guild + "/invite", async (req, res) => {
       if (config.web.endpoints.invite === false) return;
       const username = req.body.username;
+      const token = req.body.token;
+
+      if (config.minecraft.API.SCF.key !== token) return;
       let success = false;
       const uuid = await getUUID(username);
       const skykings_scammer = await Skykings.lookupUUID(uuid);
       const blacklisted = await Blacklist.checkBlacklist(uuid);
-      if (skykings_scammer !== true && blacklisted !== true) {
+      const scf_blacklisted = await SCFBlacklist.checkBlacklist(uuid);
+      if (skykings_scammer !== true && blacklisted !== true && scf_blacklisted !== true) {
         bot.chat(`/guild invite ${username}`);
         success = true;
       }
@@ -38,6 +43,9 @@ class EndpointHandler {
       if (config.web.endpoints.kick === false) return;
       const username = req.body.username;
       const reason = req.body.reason;
+      const token = req.body.token;
+
+      if (config.minecraft.API.SCF.key !== token) return;
       let success = false;
       bot.chat("/g kick " + username + " " + reason);
       success = true;
