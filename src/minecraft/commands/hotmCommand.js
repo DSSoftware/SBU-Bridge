a4478d10a1c7858fd8cd087d846610b185d12627
DS Software <artemsoftware@mail.ru>
2024-04-21 23:09:27 +0400

    help me :pray:

--- src/minecraft/commands/hotmCommand.js
+++ src/minecraft/commands/hotmCommand.js
@@ -122,8 +122,8 @@ class MedalsCommand extends minecraftCommand {
       hotm_names += name + " ";
     }
 
-    hotm_symbols = (hotm_symbols.trim().padStart(Math.max(hotm_symbols.length + Math.floor((41 - hotm_symbols.length) / 2), hotm_symbols.length), ' ').padEnd(41, ' ')).replace(" ", "§7 ");
-    hotm_names = (hotm_names.trim().padStart(Math.max(hotm_names.length + Math.floor((41 - hotm_names.length) / 2), hotm_names.length), ' ').padEnd(41, ' ')).replace(" ", "§7 ");
+    hotm_symbols = (hotm_symbols.trim().padStart(Math.max(hotm_symbols.length + Math.floor((41 - hotm_symbols.length) / 2), hotm_symbols.length), ' ').padEnd(41, ' ')).replace(/ /g,"§7 ");
+    hotm_names = (hotm_names.trim().padStart(Math.max(hotm_names.length + Math.floor((41 - hotm_names.length) / 2), hotm_names.length), ' ').padEnd(41, ' ')).replace(/ /g,"§7 ");
     
     console.log(hotm_symbols);
     console.log(hotm_names);
