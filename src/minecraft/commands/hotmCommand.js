beafb6e4aad4c0728f6f2c7c82763c7e4887a0ec
DS Software <artemsoftware@mail.ru>
2024-04-21 19:27:11 +0400

    hotm command!!!

--- src/minecraft/commands/hotmCommand.js
+++ src/minecraft/commands/hotmCommand.js
@@ -20,22 +20,83 @@ class MedalsCommand extends minecraftCommand {
     ];
   }
 
+  async getHOTMLevel(experience){
+    const HOTM_XP = {
+      1: 0,
+      2: 3000,
+      3: 9000,
+      4: 25000,
+      5: 60000,
+      6: 100000,
+      7: 150000,
+      8: 210000,
+      9: 290000,
+      10: 400000,
+    };
+
+    let left_exp = 0;
+
+    let level = 0;
+    let xp_remaining = 0;
+    let xp_to_next = 0;
+    let next_level = null;
+
+    Object.entries(HOTM_XP).forEach((level) => {
+      let hotm_level = level[0];
+      let hotm_exp = level[1];
+
+      if(left_exp >= hotm_exp){
+        left_exp -= hotm_exp;
+        level = hotm_level;
+      }
+      else{
+        xp_remaining = left_exp;
+        next_level = hotm_level;
+        xp_to_next = hotm_exp;
+      }
+    });
+
+    return {
+      level: level,
+      next_level: next_level,
+      xp_left: xp_remaining,
+      xp_to_next: xp_to_next
+    };
+  }
+
   async onCommand(username, message, channel = "gc") {
     try {
-      /*username = this.getArgs(message)[0] || username;
+      username = this.getArgs(message)[0] || username;
       const data = await getLatestProfile(username);
       username = formatUsername(username, data.profileData?.game_mode);
 
       let hotm_data = data?.profile?.mining_core;
 
-      let hotm_level = 
-
-      if (jacob_data == undefined) {
-        throw "Player hasn't participated in any Jacob's Contests.";
+      if (hotm_data == undefined) {
+        throw "Player has no HOTM data.";
       }
 
-      let Name = `§6${username}'s Jacob's Contests:`;
+      let hotm_exp = hotm_data?.experience;
+      let hotm_level_data = getHOTMLevel(hotm_exp);
+
+      let Name = `§6${username}'s HOTM Stats:`;
       let Lore = [];
+
+      Lore.push(`§f`);
+
+      Lore.push(`§7HOTM Level: ${hotm_level_data?.level} (${hotm_exp} EXP)`);
+      Lore.push(`§7Next Level: ${
+        hotm_level_data?.next_level == null ? "§a§lMAXED" : `${hotm_level_data?.next_level} (${hotm_level_data?.next_level.xp_left} / ${hotm_level_data?.next_level.xp_to_next})`
+      }`);
+
+      Lore.push(`§f`);
+
+      const renderedItem = await renderLore(Name, Lore);
+      const upload = await uploadImage(renderedItem);
+      this.send(`/${channel} ${username}'s HOTM stats: ${upload.data.link}.`);
+      /*
+
+      
 
       let medals_inv = jacob_data?.medals_inv ?? {};
       let unique_brackets = jacob_data?.unique_brackets ?? {};
@@ -78,10 +139,7 @@ class MedalsCommand extends minecraftCommand {
       Lore.unshift(`§7Unique golds: §6${golds}§7/10.`);
       Lore.unshift(`§7Participated in §6${Object.entries(contests).length}§7 contests.`);
       Lore.unshift(`§f`);
-
-      const renderedItem = await renderLore(Name, Lore);
-      const upload = await uploadImage(renderedItem);
-      this.send(`/${channel} ${username}'s Jacobs Contest stats: ${upload.data.link}.`);*/
+      */
     } catch (error) {
       console.log(error);
       this.send(`/${channel} [ERROR] ${error}`);
