c798d3dfb1baa2d1d8808ce2ae8bb070383a4c99
DS Software <artemsoftware@mail.ru>
2024-06-14 22:31:45 +0400

    added AP command

---
+++ src/minecraft/commands/activityCommand.js
@@ -0,0 +1,77 @@
+const minecraftCommand = require('../../contracts/minecraftCommand.js');
+const { getUUID } = require('../../contracts/API/PlayerDBAPI.js');
+const axios = require('axios');
+const config = require('../../../config.js');
+const { renderLore } = require('../../contracts/renderItem.js');
+const { uploadImage } = require('../../contracts/API/imgurAPI.js');
+
+class topCommand extends minecraftCommand {
+    constructor(minecraft) {
+        super(minecraft);
+
+        this.name = 'activity';
+        this.aliases = ['points', 'ap'];
+        this.description = 'Sends your Activity Points';
+        this.options = [
+            {
+                name: 'username',
+                description: 'Minecraft username',
+                required: false
+            }
+        ];
+    }
+
+    async onCommand(username, message, channel = 'gc') {
+        try {
+            if (!config.minecraft.API.SCF.enabled) {
+                return this.send(`/${channel} This command was disabled!`);
+            }
+
+            username = this.getArgs(message)[0] || username;
+
+            let score = 0;
+            let gexp = 0;
+
+            const player_uuid = await getUUID(username);
+
+            const guild = await hypixel.getGuild('player', player_uuid);
+            if(guild?.members == undefined){
+                throw "Wrong IGN or player is not in a guild!";
+            }
+            const player = (guild?.members ?? {}).find((member) => member.uuid == player_uuid);
+
+            if (player === undefined) {
+                throw 'Player is not in the Guild.';
+            }
+
+            gexp = player.weeklyExperience || 0;
+
+            let placement_info = await Promise.all([
+                axios.get(
+                    `https://sky.dssoftware.ru/api.php?method=getMessagesSent&uuid=${player_uuid}&api=${config.minecraft.API.SCF.key}&overall=1`
+                )
+            ]).catch((error) => {});
+
+            placement_info = placement_info[0].data ?? {};
+
+            score = placement_info.data.count ?? 0;
+
+            const aps = gexp + 500 * score;
+
+            this.send(
+                `/${channel} ${username}'s Activity Points: ${aps} (GEXP: ${gexp}, Score: ${score})`
+            );
+        } catch (error) {
+            console.log(error);
+            this.send(
+                `/${channel} [ERROR] ${error
+                    .toString()
+                    .replace('[hypixel-api-reborn] ', '')
+                    .replace('For help join our Discord Server https://discord.gg/NSEBNMM', '')
+                    .replace('Error:', '[ERROR]')}`
+            );
+        }
+    }
+}
+
+module.exports = topCommand;
