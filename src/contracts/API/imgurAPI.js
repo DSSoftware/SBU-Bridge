d38a3839f7452805f6a23c1fcc34f1c8235029e2
DS Software <artemsoftware@mail.ru>
2024-06-22 22:40:02 +0400

    erm

--- src/contracts/API/imgurAPI.js
+++ src/contracts/API/imgurAPI.js
@@ -11,6 +11,7 @@ async function uploadImage(image) {
     });
 
     if (response.success === false) {
+        console.log(response);
         // eslint-disable-next-line no-throw-literal
         throw 'An error occured while uploading the image. Please try again later.';
     }
