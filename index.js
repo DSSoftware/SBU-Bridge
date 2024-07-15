9b9c7c38f4b357240f9a329d139ba14b11221ba3
DS Software <artemsoftware@mail.ru>
2024-07-16 02:22:51 +0400

    feat: now pings @here at fail

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ if (cluster.isPrimary) {
     function messageHandler(message) {
         if (message.event_id && message.event_id === 'exceptionCaught') {
             var params = {
+                content: "@here",
                 embeds: [
                     {
                         title: 'Bot Failed',
