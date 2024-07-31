21307911bd2350663732e42c146d096343189ecb
DS Software <artemsoftware@mail.ru>
2024-08-01 02:15:23 +0400

    added correct allowed bot

--- config.js
+++ config.js
@@ -147,7 +147,7 @@ module.exports = {
             loggingChannel: process.env.channel_logging,
             debugMode: true,
             debugChannel: process.env.channel_debug,
-            allowedBots: ['155149108183695360', '1224056601829441619', '1049379596006588417']
+            allowedBots: ['155149108183695360', '1224056601829441619', '1049379596006588417', '1268311020816826398']
         },
         replication: {
             enabled: process.env.replica_enabled == 'true',
