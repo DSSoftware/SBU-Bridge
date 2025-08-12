ce613e7e0d38615836861f8bfe6bc270751dd346
DS Software <artemsoftware@mail.ru>
2025-08-12 18:32:55 +0400

    player.socialMedia

--- src/discord/commands/linkCommand.js
+++ src/discord/commands/linkCommand.js
@@ -31,8 +31,8 @@ module.exports = {
             `https://api.hypixel.net/v2/player?key=${config.API.hypixelAPIkey}&uuid=${uuid}`
         ).catch((error) => {});
 
-        console.log(hypixel_info.socialMedia);
-        let tag = hypixel_info.socialMedia?.links?.DISCORD || undefined;
+        console.log(hypixel_info.player.socialMedia);
+        let tag = hypixel_info.player.socialMedia?.links?.DISCORD || undefined;
 
         if (tag != user.user.username) {
             throw new HypixelDiscordChatBridgeError(
