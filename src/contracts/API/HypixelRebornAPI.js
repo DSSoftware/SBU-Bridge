a45fd2943c38ed2d4c19e956cf0abcd7725c6b0e
ARKTD <support@dssoftware.ru>
2025-09-09 12:36:42 +0500

    huh whats wrong with config then

--- src/contracts/API/HypixelRebornAPI.js
+++ src/contracts/API/HypixelRebornAPI.js
@@ -1,7 +1,7 @@
 const HypixelAPIReborn = require('hypixel-api-reborn');
 const config = require('#root/config.js').getConfig();
 
-console.log(config.API.hypixelAPIKey);
+console.log(config);
 
 const hypixel = new HypixelAPIReborn.Client(config.API.hypixelAPIkey, {
     cache: true
