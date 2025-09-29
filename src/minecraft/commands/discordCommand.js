ac5b9386f794e6e392a4e1fdd8650038f736c832
chris <chris.ruell@rubic-solution.de>
2025-09-29 18:30:15 +0200

    changed !discord command

--- src/minecraft/commands/discordCommand.js
+++ src/minecraft/commands/discordCommand.js
@@ -13,7 +13,8 @@ class DiscordLinkCommand extends minecraftCommand {
 
     onCommand(username, message, channel = 'gc') {
         try {
-            this.send(`/${channel} Join our discord - ${config.minecraft.guild.discord_invite}`);
+            //this.send(`/${channel} Join our discord - ${config.minecraft.guild.discord_invite}`);
+            this.send(/${channel} Disabled!, please use /g discord instead);
         } catch (error) {
             this.send(`/${channel} Something went wrong...`);
         }
