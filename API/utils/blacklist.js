4688d55b2be299cc27676c32f83b998eff92d331
chris <chris.ruell@rubic-solution.de>
2025-10-18 12:56:40 +0200

    updated blacklist

--- API/utils/blacklist.js
+++ API/utils/blacklist.js
@@ -1,36 +1,29 @@
 const config = require('#root/config.js').getConfig();
 const axios = require('axios');
+const Logger = require('../../src/Logger');
 
 async function checkBlacklist(uuid) {
-    return new Promise(async (resolve, reject) => {
+    return new Promise(async (resolve) => {
         try {
             if (config.API.banlist.enabled === false) {
                 resolve(false);
                 return;
             }
-            const API_URL = config.API.banlist.URL;
 
-            await axios.get(API_URL, {
+            const API_URL = `${config.API.banlist.URL}/${uuid}`;
+            const response = await axios.get(API_URL, {
                 headers: {
-                    'Content-type': 'application/json'
+                    'Content-type': 'application/json',
+                    Authorization: `Bearer ${config.API.SBU.key}`
                 }
             });
 
-            let blacklisted = false;
-
-            axios
-                .get(API_URL + `?uuid=${uuid}`)
-                .then(function (response) {
-                    if (response.data.banned) {
-                        blacklisted = true;
-                    }
-                    resolve(blacklisted);
-                })
-                .catch(function (error) {
-                    blacklisted = true;
-                    resolve(blacklisted);
-                });
-        } catch (e) {
+            resolve(response.data?.banned === true);
+        } catch (error) {
+            if (error.response?.status !== 404) {
+                // Log error only if it's not a 404 (user not found)
+                Logger.warnMessage(`Error checking blacklist for ${uuid}: ${error.message}`);
+            }
             resolve(false);
         }
     });
