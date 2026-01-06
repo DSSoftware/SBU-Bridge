3202a49f6edc48e3ce0449b975d390f7482c2d12
Artem <ytartemsd@gmail.com>
2026-01-06 16:51:23 +0500

    improved credits command

--- src/discord/commands/creditsCommand.js
+++ src/discord/commands/creditsCommand.js
@@ -10,13 +10,13 @@ module.exports = {
 
         // Developers
         used_sources += "**Developers**\n";
-        used_sources += "Artem (Discord: @artemdev)\n";
-        used_sources += "Rubiclex (Discord: @rubiclex)\n";
+        used_sources += "Artem (<@476365125922586635>)\n";
+        used_sources += "Rubiclex (<@199351956320419840>)\n";
 
         // Used Projects
         used_sources += "\n**Used Projects**\n";
-        used_sources += "[DuckySoLucky's Bridge](https://github.com/duckysolucky/hypixel-discord-chat-bridge)\n";
-        used_sources += "[SCF API](https://github.com/SCF-Tools/SCF-API-Client)\n";
+        used_sources += "A fork of [DuckySoLucky's Bridge](https://github.com/duckysolucky/hypixel-discord-chat-bridge)\n";
+        used_sources += "Uses [SCF API](https://github.com/SCF-Tools/SCF-API-Client)\n";
 
         let embed = new EmbedBuilder()
             .setColor(5763719)
