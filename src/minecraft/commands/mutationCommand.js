238391f5749a1ba0ff42670da7ed7d4c49b823e0
chris <chris.ruell@rubic-solution.de>
2026-02-24 20:57:27 +0100

    test mutation cmd

---
+++ src/minecraft/commands/mutationCommand.js
@@ -0,0 +1,43 @@
+const minecraftCommand = require('../../contracts/minecraftCommand.js');
+const { formatNumber, formatUsername } = require('../../contracts/helperFunctions.js');
+const { getLatestProfile } = require('../../../API/functions/getLatestProfile.js');
+const Logger = require('#root/src/Logger.js');
+
+class MutationCommand extends minecraftCommand {
+    constructor(minecraft) {
+        super(minecraft);
+
+        this.name = 'mutation';
+        this.aliases = ['mutation', 'mut'];
+        this.description = 'Skyblock Garden Mutation Stats of specified user.';
+        this.options = [
+            {
+                name: 'username',
+                description: 'Minecraft username',
+                required: false
+            }
+        ];
+    }
+
+    async onCommand(username, message, channel = 'gc') {
+        try {
+            username = this.getArgs(message)[0] || username;
+
+            const data = await getLatestProfile(username);
+
+            username = formatUsername(username, data.profileData?.game_mode);
+
+
+
+            this.send(
+                `/${channel} ${username} has x / 40 mutations unlocked`
+            );
+        } catch (error) {
+            Logger.warnMessage(error);
+
+            this.send(`/${channel} [ERROR] ${error}`);
+        }
+    }
+}
+
+module.exports = MutationCommand;
