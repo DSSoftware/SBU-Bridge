5cbedbbc06e008e18633b78785ae3eb69373ac6c
chris <chris.ruell@rubic-solution.de>
2026-02-25 15:10:58 +0100

    fixed display

--- src/minecraft/commands/visitorCommand.js
+++ src/minecraft/commands/visitorCommand.js
@@ -8,7 +8,7 @@ class VisitorCommand extends minecraftCommand {
         super(minecraft);
 
         this.name = 'visitor';
-        this.aliases = ['visitor', 'visit'];
+        this.aliases = ['visitor'];
         this.description = 'Shows Garden Visitor Stats of a User.';
         this.options = [
             {
@@ -31,7 +31,7 @@ class VisitorCommand extends minecraftCommand {
             const count_vis = data.garden.commission_data.total_completed;
 
             this.send(
-                `/${channel} ${username} has ${count_vis} Visitors served and ${uni_vis} unique ones. `
+                `/${channel} ${username} has ${count_vis} visitors served | -> ${uni_vis} unique ones. `
             );
         } catch (error) {
             Logger.warnMessage(error);
