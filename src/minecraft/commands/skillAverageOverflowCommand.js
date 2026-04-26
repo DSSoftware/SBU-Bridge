c52b19dc0e98c80249757184e7e012316ad62bc8
chris <chris.ruell@rubic-solution.de>
2026-04-26 11:46:10 +0200

    added !sao command update10

--- src/minecraft/commands/skillAverageOverflowCommand.js
+++ src/minecraft/commands/skillAverageOverflowCommand.js
@@ -1,5 +1,5 @@
 const minecraftCommand = require('../../contracts/minecraftCommand.js');
-const { formatUsername } = require('../../contracts/helperFunctions.js');
+const { formatUsername, formatNumber } = require('../../contracts/helperFunctions.js');
 const { getLatestProfile } = require('../../../API/functions/getLatestProfile.js');
 const { getUUID } = require('../../contracts/API/PlayerDBAPI.js');
 const getSkills = require('../../../API/stats/skills.js');
@@ -45,14 +45,14 @@ class SkillAverageOverflowCommand extends minecraftCommand {
                     sa_skills++;
 
                     const skillName = skill[0].toUpperCase() + skill.slice(1);
-                    return `${skillName} ${level}`;
+                    return `${skillName} ${formatNumber(level)}`;
                 })
                 .join(' | ');
 
             let skillAverage = 'N/A';
 
             if (sa_skills != 0) {
-                skillAverage = (sa_points / sa_skills).toFixed(2);
+                skillAverage = formatNumber(sa_points / sa_skills);
             }
 
             this.send(`/${channel} ${username}'s Skill Average: ${skillAverage ?? 0} (${skillsFormatted})`);
