ba95319adb80d4cf7ba21d84ab357892088ce2e6
chris <chris.ruell@rubic-solution.de>
2026-07-20 09:39:30 +0200

    fixed nw to skykings endpoint

--- API/utils/skykings.js
+++ API/utils/skykings.js
@@ -26,6 +26,37 @@ async function lookupUUID(uuid) {
     }
 }
 
+async function getNetworth(profileData, uuid, options = {}) {
+    try {
+        const response = await axios.post(
+            'https://api.skykings.net/networth',
+            {
+                profile_data: profileData,
+                uuid: uuid,
+                profile_name: options.profileName,
+                top: options.top ?? 25,
+                include_bank: options.includeBank ?? true,
+                include_all_entries: options.includeAllEntries ?? false
+            },
+            {
+                headers: {
+                    Authorization: key
+                }
+            }
+        );
+
+        if (response.data?.success !== true) {
+            throw new Error(response.data?.message ?? 'SkyKings networth lookup failed.');
+        }
+        console.log(response.data.data);
+        return response.data.data;
+    } catch (error) {
+        Logger.warnMessage(error?.response?.data ?? error);
+        throw new Error(error?.response?.data?.message ?? error.message ?? 'SkyKings networth lookup failed.');
+    }
+}
+
 module.exports = {
-    lookupUUID
+    lookupUUID,
+    getNetworth
 };
